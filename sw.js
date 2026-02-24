24f57d0d64698bcccd2ee99dbd68a3f6fae19fee
mcembalagensbh <mcembalagensbh@gmail.com>
2026-02-24 16:17:06 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+// sw.js - Service Worker do HCD Life
+const CACHE_NAME = 'hcdlife-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker ativado');
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+  console.log('Notificação clicada');
+  event.notification.close();
+  
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true })
+      .then((clientList) => {
+        if (clientList.length > 0) {
+          return clientList[0].focus();
+        }
+        return clients.openWindow('/');
+      })
+  );
+});
